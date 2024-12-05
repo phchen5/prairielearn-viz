6a41d6bd0b7f2d6a44e074658052d8b351b32eef
Ben Chen <benjamin990126@hotmail.com>
2024-12-04 23:06:31 -0800

    remove all extensions to test

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@ author = u"Ben Chen"
 # ones.
 extensions = [
     # "myst_nb",
-    "autoapi.extension",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.viewcode",
+    # "autoapi.extension",
+    # "sphinx.ext.napoleon",
+    # "sphinx.ext.viewcode",
 ]
 autoapi_dirs = ["../src"]
 
