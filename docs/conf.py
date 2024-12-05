65b36e9146a9c11b8d0b37a342f3c8bde4ce8d90
Ben Chen <benjamin990126@hotmail.com>
2024-12-04 23:21:28 -0800

    reinstall extensions attempt

--- docs/conf.py
+++ docs/conf.py
@@ -16,11 +16,12 @@ author = u"Ben Chen"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    # "myst_nb",
-    # "autoapi.extension",
-    # "sphinx.ext.napoleon",
-    # "sphinx.ext.viewcode",
+    "myst_nb",
+    "autoapi.extension",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
 ]
+
 autoapi_dirs = ["../src"]
 
 # List of patterns, relative to source directory, that match files and
@@ -33,7 +34,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Adding logo
 # html_static_path = ['_static']
